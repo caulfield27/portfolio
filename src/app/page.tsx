e2aa9ae5b9e98cee289531e919c22bd73d8d8b31
holden <al1onggwp@gmail.com>
2024-06-18 22:23:23 +0500

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,26 @@ import LinkButton from "@/shared/linkButton/linkButton"
 export default function Home() {
   return (
     <>
-      <section className={styles.left_section}>
-        <img className={styles.vector} src="/Vector.png" alt="vector"/>
-        <div className={styles.left_text}>
-          <p>MY NAME<br/>IS <b>SALIKHOV ALISHER...</b></p>
-          <h3>Front-end developer</h3>
-        </div>
-        <LinkButton label="Let's talk with me" link="https://t.me/salikhov1337" isLoad={false}/>
-        <div className={styles.contacts_wrap}>
-          <MyContacts icon="/phone.svg" contact="+992 005 88 02 85"/>
-          <MyContacts icon="/email.svg" contact="al1onggwp@gmail.com"/>
-          
-        </div>
-      </section>
-      <section className={styles.right_section}>
+      <div className={styles.home_container}>
+        <section className={styles.left_section}>
+          <img className={styles.vector} src="/Vector.png" alt="vector" />
+          <div className={styles.left_text}>
+            <p>MY NAME<br />IS <b>SALIKHOV ALISHER...</b></p>
+            <h3>Front-end developer</h3>
+          </div>
+          <LinkButton label="Let's talk with me" link="https://t.me/salikhov1337" isLoad={false} />
+          <div className={styles.contacts_wrap}>
+            <MyContacts icon="/phone.svg" contact="+992 005 88 02 85" />
+            <MyContacts icon="/email.svg" contact="al1onggwp@gmail.com" />
 
-      </section>
+          </div>
+        </section>
+        <section className={styles.right_section}>
+          <img src="/dude.png" alt="dude" />
+
+        </section>
+      </div>
+
     </>
   )
 }
